6b8636b17ccf98738c21cb8a7731b27b58b1e8b4
Kelvin Rodrigues <krodrigues@first.pt>
2023-01-23 15:58:43 +0000

    Open book

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -39,7 +39,7 @@ class _HomeState extends State<Home> {
                     color: Colors.white,
                   ),
                 )
-              : Book(path: file.path),
+              : Book(path: file),
         ],
       ),
     );
