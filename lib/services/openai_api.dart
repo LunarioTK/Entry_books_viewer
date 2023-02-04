224052a863a11d9e000f13f7a9642929c115be2f
Kelvin Rodrigues <81050267+LunarioTK@users.noreply.github.com>
2023-02-04 19:02:00 +0000

    Updates

--- lib/services/openai_api.dart
+++ lib/services/openai_api.dart
@@ -1,5 +1,5 @@
 class APIKey {
-  final String _apiKey = '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';
+  final String _apiKey = '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';
 
   String get getApiKey {
     return _apiKey;
