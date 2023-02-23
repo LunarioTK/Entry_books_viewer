51db9aadb5b94c0db1a030e85b36d5d6b8b09625
Kelvin Rodrigues <kelvin-rodrigues2012@hotmail.com>
2023-02-23 17:45:00 +0000

    Code update

--- lib/constants/panelplayer.dart
+++ lib/constants/panelplayer.dart
@@ -1,1 +1,391 @@
+import 'dart:async';
 
+import 'package:audioplayers/audioplayers.dart';
+import 'package:entry_books/services/bookinfo.dart';
+import 'package:entry_books/services/playtts.dart';
+import 'package:pdf_render/pdf_render_widgets.dart';
+import 'package:provider/provider.dart';
+import 'package:sliding_up_panel/sliding_up_panel.dart';
+import 'package:audio_video_progress_bar/audio_video_progress_bar.dart';
+
+import 'package:rxdart/rxdart.dart';
+import 'package:flutter/material.dart';
+
+class PlayerWidget extends StatefulWidget {
+  final AudioPlayer player;
+  final bool isAudioLoaded;
+  final ScrollController controller;
+  final PanelController panelController;
+
+  const PlayerWidget(
+      {super.key,
+      required this.player,
+      required this.controller,
+      required this.isAudioLoaded,
+      required this.panelController});
+
+  @override
+  State<StatefulWidget> createState() {
+    return _PlayerWidgetState();
+  }
+}
+
+class _PlayerWidgetState extends State<PlayerWidget> {
+  final debouncer = PublishSubject<double>();
+  final double _value = 0.0;
+  double _sliderValue = 0.0;
+
+  PlayerState? _playerState;
+  Duration? _duration;
+  Duration? _position;
+
+  StreamSubscription? _durationSubscription;
+  StreamSubscription? _positionSubscription;
+  StreamSubscription? _playerCompleteSubscription;
+  StreamSubscription? _playerStateChangeSubscription;
+
+  bool get _isPlaying => _playerState == PlayerState.playing;
+
+  bool get _isPaused => _playerState == PlayerState.paused;
+
+  String get _durationText => _duration?.toString().split('.').first ?? '';
+
+  String get _positionText => _position?.toString().split('.').first ?? '';
+
+  AudioPlayer get player => widget.player;
+  PanelController get panelController => widget.panelController;
+
+  // Play button
+  Widget playButton() {
+    bool isAudioLoaded =
+        Provider.of<TtsPlayer>(context, listen: false).isAudioLoaded;
+    return isAudioLoaded == false
+        ? Container(
+            margin: const EdgeInsets.all(10.0),
+            width: 25.0,
+            height: 25.0,
+            child: const CircularProgressIndicator(
+              color: Colors.grey,
+            ),
+          )
+        : IconButton(
+            key: const Key('play_button'),
+            onPressed: _isPlaying && isAudioLoaded ? null : _play,
+            iconSize: 40.0,
+            icon: Icon(
+              Icons.play_arrow,
+              color: !_isPlaying && isAudioLoaded ? Colors.black : Colors.grey,
+            ),
+            color: Colors.grey,
+          );
+  }
+
+  // Pause button
+  Widget pauseButton() {
+    bool isAudioLoaded =
+        Provider.of<TtsPlayer>(context, listen: false).isAudioLoaded;
+    return IconButton(
+      key: const Key('pause_button'),
+      onPressed: _isPlaying && isAudioLoaded ? _pause : null,
+      iconSize: 40.0,
+      icon: Icon(
+        Icons.pause,
+        color: _isPlaying ? Colors.black : Colors.grey,
+      ),
+    );
+  }
+
+  // Stop button
+  Widget stopButton() {
+    bool isAudioLoaded =
+        Provider.of<TtsPlayer>(context, listen: false).isAudioLoaded;
+
+    return IconButton(
+      key: const Key('stop_button'),
+      onPressed: _isPlaying && isAudioLoaded || _isPaused && isAudioLoaded
+          ? _stop
+          : null,
+      iconSize: 40.0,
+      icon: Icon(
+        Icons.stop,
+        color: _isPlaying && isAudioLoaded || _isPaused && isAudioLoaded
+            ? Colors.black
+            : Colors.grey,
+      ),
+      //color: Colors.grey,
+    );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    // Use initial values from player
+    _playerState = player.state;
+    /*player.getDuration().then(
+          (value) => setState(() {
+            _duration = value;
+          }),
+        );
+    player.getCurrentPosition().then(
+          (value) => setState(() {
+            _position = value;
+          }),
+        );*/
+    _initStreams();
+  }
+
+  @override
+  void setState(VoidCallback fn) {
+    // Subscriptions only can be closed asynchronously,
+    // therefore events can occur after widget has been disposed.
+    if (mounted) {
+      super.setState(fn);
+    }
+  }
+
+  @override
+  void dispose() {
+    _durationSubscription?.cancel();
+    _positionSubscription?.cancel();
+    _playerCompleteSubscription?.cancel();
+    _playerStateChangeSubscription?.cancel();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final panelHeight = MediaQuery.of(context).size.height;
+    return ListView(
+      controller: widget.controller,
+      padding: EdgeInsets.zero,
+      shrinkWrap: true,
+      children: <Widget>[
+        const SizedBox(height: 12),
+        buildHandle(),
+        buildPlayer(panelHeight),
+      ],
+    );
+  }
+
+  // Trace
+  Widget buildHandle() => GestureDetector(
+        onTap: (() => togglePanel()),
+        child: Center(
+          child: Container(
+              width: 40,
+              height: 7,
+              decoration: BoxDecoration(
+                color: Colors.grey[300],
+                borderRadius: const BorderRadius.all(Radius.circular(12)),
+              )),
+        ),
+      );
+
+  void togglePanel() {
+    panelController.isPanelOpen
+        ? panelController.close()
+        : panelController.open();
+  }
+
+  Widget buildPlayer(double heightSize) {
+    return Padding(
+      padding:
+          EdgeInsets.only(top: heightSize * 0.10, bottom: heightSize * 0.05),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          const BookThumbnail(),
+          SizedBox(height: heightSize <= 600 ? 10 : 30),
+          const Text(
+            'Book Name',
+            style: TextStyle(
+              fontSize: 25,
+              fontWeight: FontWeight.bold,
+              letterSpacing: 1.0,
+            ),
+          ),
+          const SizedBox(height: 10),
+          const Text('Author Name'),
+          SizedBox(height: heightSize <= 600 ? 10 : 30),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              pauseButton(),
+              playButton(),
+              stopButton(),
+            ],
+          ),
+          Padding(
+            padding: heightSize <= 600
+                ? const EdgeInsets.only(left: 40, right: 40)
+                : const EdgeInsets.only(left: 50, right: 50, top: 30),
+            child: ProgressBar(
+              total: _duration ?? Duration.zero,
+              baseBarColor: Colors.grey,
+              thumbColor: Colors.black,
+              progressBarColor: Colors.black,
+              onSeek: (time) {
+                player
+                    .seek(Duration(milliseconds: time.inMilliseconds.round()));
+              },
+              progress: Duration(
+                milliseconds: (_position != null &&
+                        _duration != null &&
+                        _position!.inMilliseconds > 0 &&
+                        _position!.inMilliseconds < _duration!.inMilliseconds)
+                    ? _position!.inMilliseconds
+                    : 0,
+              ),
+            ),
+          ),
+
+          /*Slider(
+            activeColor: Colors.green.shade800,
+            onChanged: (v) {
+              debouncer.add(v);
+            },
+            value: _sliderValue,
+          ),*/
+
+          /*Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  _position != null
+                      ? _positionText
+                      : _duration != null
+                          ? _durationText
+                          : '',
+                  style: const TextStyle(fontSize: 16.0),
+                ),
+                Text(
+                  _position != null
+                      ? _durationText
+                      : _duration != null
+                          ? _durationText
+                          : '',
+                  style: const TextStyle(fontSize: 16.0),
+                ),
+              ],
+            ),
+          ),*/
+        ],
+      ),
+    );
+  }
+
+  /*_PlayerWidgetState() {
+    debouncer.stream
+        .debounceTime(const Duration(milliseconds: 500))
+        .listen((value) {
+      if (_duration != null) {
+        final position = _duration! * value;
+        player.seek(position);
+      }
+    });
+  }*/
+
+  void _initStreams() {
+    _positionSubscription =
+        player.onPositionChanged.listen((Duration position) {
+      setState(() {
+        _position = position;
+        if (_duration != null) {
+          _sliderValue = (_position!.inMilliseconds / _duration!.inMilliseconds)
+              .clamp(0.0, 1.0);
+        }
+      });
+    });
+
+    _durationSubscription =
+        player.onDurationChanged.listen((Duration duration) {
+      setState(() {
+        _duration = duration;
+        if (_position != null) {
+          _sliderValue = (_position!.inMilliseconds / _duration!.inMilliseconds)
+              .clamp(0.0, 1.0);
+        }
+      });
+    });
+
+    /*_durationSubscription = player.onDurationChanged.listen((duration) {
+      setState(() => _duration = duration);
+    });
+
+    _positionSubscription = player.onPositionChanged.listen(
+      (p) => setState(() => _position = p),
+    );*/
+
+    _playerCompleteSubscription = player.onPlayerComplete.listen((event) {
+      setState(() {
+        _playerState = PlayerState.stopped;
+        _position = Duration.zero;
+      });
+    });
+
+    _playerStateChangeSubscription =
+        player.onPlayerStateChanged.listen((state) {
+      setState(() {
+        _playerState = state;
+      });
+    });
+  }
+
+  Future<void> _play() async {
+    final position = _position;
+    if (position != null && position.inMilliseconds > 0) {
+      await player.seek(position);
+    }
+    await player.resume();
+    setState(() => _playerState = PlayerState.playing);
+  }
+
+  Future<void> _pause() async {
+    await player.pause();
+    setState(() => _playerState = PlayerState.paused);
+  }
+
+  Future<void> _stop() async {
+    await player.stop();
+    setState(() {
+      _playerState = PlayerState.stopped;
+      _position = Duration.zero;
+    });
+  }
+}
+
+// Book Thumbnail
+class BookThumbnail extends StatelessWidget {
+  const BookThumbnail({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    var bookInfo = context.watch<BookInfo>();
+    final panelHeight = MediaQuery.of(context).size.height;
+
+    return Container(
+      key: const Key('Thumbnail_container'),
+      height: panelHeight <= 600 ? 160 : 300,
+      width: panelHeight <= 600 ? 240 : 300,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(20),
+        color: Colors.blue,
+      ),
+      child: PdfDocumentLoader.openFile(
+        bookInfo.getFile.path,
+        key: const Key('Thumbnail_panel'),
+        pageNumber: 1,
+        pageBuilder: (context, textureBuilder, pageSize) => textureBuilder(
+          size: Size(
+            panelHeight <= 600 ? 240 : 300, // width
+            panelHeight <= 600 ? 160 : 300, // heigth
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/constants/ttsplayer.dart
+++ lib/constants/ttsplayer.dart
@@ -1,1 +1,349 @@
+import 'dart:async';
+import 'dart:io';
 
+import 'package:audioplayers/audioplayers.dart';
+import 'package:entry_books/constants/uicolor.dart';
+import 'package:entry_books/services/azureapi.dart';
+import 'package:entry_books/services/bookinfo.dart';
+import 'package:entry_books/services/getresponse.dart';
+import 'package:entry_books/services/gettext.dart';
+import 'package:entry_books/services/panelstate.dart';
+import 'package:entry_books/services/playtts.dart';
+import 'package:flutter/material.dart';
+import 'package:pdf_render/pdf_render_widgets.dart' as render;
+import 'package:http/http.dart' as http;
+import 'package:path_provider/path_provider.dart';
+
+import 'package:provider/provider.dart';
+import 'package:sliding_up_panel/sliding_up_panel.dart';
+
+class TTSPlayer extends StatefulWidget {
+  PanelController panelController = PanelController();
+  File file;
+  TTSPlayer({super.key, required this.file, required this.panelController});
+
+  @override
+  State<TTSPlayer> createState() => _TTSPlayerState();
+}
+
+class _TTSPlayerState extends State<TTSPlayer> {
+  AudioPlayer audioPlayer = AudioPlayer();
+  TtsPlayer playTts = TtsPlayer();
+
+  late final render.PdfDocumentLoader pdfThumbnail =
+      render.PdfDocumentLoader.openFile(
+    widget.file.path,
+    pageNumber: 1,
+    pageBuilder: (context, textureBuilder, pageSize) => textureBuilder(
+      backgroundFill: true,
+      size: const Size(70, 100),
+    ),
+  );
+  bool playButtonPressed = false;
+  bool isPlaying = false;
+  bool hasFinished = false;
+  IconData iconData = Icons.play_arrow_rounded;
+
+  PlayerState? _playerState;
+  Duration? _duration;
+  Duration? _position;
+
+  StreamSubscription? _durationSubscription;
+  StreamSubscription? _positionSubscription;
+  StreamSubscription? _playerCompleteSubscription;
+  StreamSubscription? _playerStateChangeSubscription;
+
+  String get _durationText => _duration?.toString().split('.').first ?? '';
+
+  String get _positionText => _position?.toString().split('.').first ?? '';
+
+  bool get _isPlaying => _playerState == PlayerState.playing;
+
+  bool get _isPaused => _playerState == PlayerState.paused;
+
+  PanelController get panelController => widget.panelController;
+
+  /*Future<List<int>> _readDocumentData(String name) async {
+    final ByteData data = await rootBundle.load(name);
+    return data.buffer.asUint8List(data.offsetInBytes, data.lengthInBytes);
+  }*/
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    // Use initial values from player
+    _playerState = audioPlayer.state;
+    _pause();
+    //audioPlayer.seek(Duration.zero);
+    audioPlayer.getDuration().then(
+          (value) => setState(() {
+            _duration = value;
+          }),
+        );
+    audioPlayer.getCurrentPosition().then(
+          (value) => setState(() {
+            _position = value;
+          }),
+        );
+    _initStreams();
+  }
+
+  @override
+  void setState(VoidCallback fn) {
+    // Subscriptions only can be closed asynchronously,
+    // therefore events can occur after widget has been disposed.
+    if (mounted) {
+      super.setState(fn);
+    }
+  }
+
+  @override
+  void dispose() {
+    _durationSubscription?.cancel();
+    _positionSubscription?.cancel();
+    _playerCompleteSubscription?.cancel();
+    _playerStateChangeSubscription?.cancel();
+    super.dispose();
+  }
+
+  void changeIcon(bool isButtonPressed) {
+    setState(() {
+      if (isButtonPressed) {
+        iconData = Icons.pause;
+      } else {
+        iconData = Icons.play_arrow_rounded;
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    GetText getText = GetText();
+    TtsPlayer playTts = context.watch<TtsPlayer>();
+    GetResponse getResponse = GetResponse();
+    MediaQueryData media = MediaQuery.of(context);
+    double height = media.size.height;
+    double width = media.size.width;
+
+    var bookInfo = context.watch<BookInfo>();
+    var isPanelOpen = Provider.of<MyPanelState>(context, listen: false);
+
+    //String? text;
+
+    void showResult(String? text) {
+      showDialog(
+          context: context,
+          builder: (BuildContext context) {
+            return AlertDialog(
+              title: const Text(
+                'Explanation',
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(15.0),
+              ),
+              backgroundColor: uiColor,
+              content: Scrollbar(
+                child: SingleChildScrollView(
+                  physics: const BouncingScrollPhysics(
+                    parent: AlwaysScrollableScrollPhysics(),
+                  ),
+                  child: Text(
+                    text!,
+                    style: const TextStyle(
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
+            );
+          });
+    }
+
+    void onPressThumbnailAzure(String text) async {
+      http.Response response = await TextToSpeechApi.textToSpeech(text);
+      final dir = await getTemporaryDirectory();
+      final file = File('${dir.path}/audio.mp3');
+
+      final bytes = response.bodyBytes;
+
+      await file.writeAsBytes(bytes);
+
+      await audioPlayer.play(DeviceFileSource(file.path));
+    }
+
+    void onPressThumbnail() async {
+      await getText.getText(bookInfo.getPageNumber, widget.file);
+
+      // If audio has finished
+      playTts.audioPlayer.onPlayerComplete.listen((event) {
+        hasFinished = true;
+        isPlaying = false;
+        playTts.audioPlayer.release();
+        setState(() {
+          playButtonPressed = false;
+          changeIcon(playButtonPressed);
+        });
+      });
+
+      playTts.audioPlayer.getDuration();
+      if (_isPlaying == false) {
+        if (_duration != null && audioPlayer.source != null) {
+          _play();
+        } else {
+          await playTts.playBook(getText.pdfText);
+          //onPressThumbnailAzure(getText.pdfText);
+          audioPlayer.play(DeviceFileSource(playTts.getAudioFile.path));
+          setState(() => _playerState = PlayerState.playing);
+        }
+      } else {
+        _pause();
+      }
+    }
+
+    // Pdf Tumbnail
+    Widget pdfTumbnail() {
+      return GestureDetector(
+        onTap: () {
+          setState(() {
+            playButtonPressed = _isPaused ? true : false;
+            changeIcon(playButtonPressed);
+          });
+          onPressThumbnail();
+        },
+        child: Container(
+          height: 70,
+          width: 60,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(15),
+          ),
+          child: Stack(
+            children: [
+              Center(
+                child: pdfThumbnail,
+              ),
+              Center(
+                child: Icon(
+                  iconData,
+                  size: 30,
+                  color: uiColor,
+                ),
+              ),
+            ],
+          ),
+        ),
+      );
+    }
+
+    //! ---------- !\\
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 15),
+      child: Material(
+        elevation: 10,
+        color: uiColor,
+        borderRadius: BorderRadius.circular(10),
+        child: GestureDetector(
+          onTap: () => widget.panelController.open(),
+          child: Container(
+            width: width,
+            height: height * 0.15,
+            decoration: BoxDecoration(
+              color: uiColor,
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: ListTile(
+              leading: Padding(
+                padding: height <= 600
+                    ? const EdgeInsets.only(bottom: 5)
+                    : const EdgeInsets.only(),
+                child: pdfTumbnail(),
+              ),
+              title: const Text(
+                'Book name',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 18,
+                ),
+              ),
+              subtitle: Text(
+                _positionText,
+                style: const TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+              trailing: IconButton(
+                onPressed: (() async {
+                  getText.getText(bookInfo.getPageNumber, widget.file);
+                  await getResponse.getResponse(getText.pdfText);
+                  showResult(getResponse.chatResponse);
+                }),
+                color: Colors.white,
+                iconSize: 30,
+                icon: const Icon(Icons.menu_book_rounded),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  void _initStreams() {
+    _durationSubscription = audioPlayer.onDurationChanged.listen((duration) {
+      setState(() {
+        _duration = duration;
+        playTts.setDuration = duration;
+      });
+    });
+
+    _positionSubscription = audioPlayer.onPositionChanged.listen(
+      (p) => setState(() {
+        _position = p;
+        playTts.setPosition = p;
+      }),
+    );
+
+    _playerCompleteSubscription = audioPlayer.onPlayerComplete.listen((event) {
+      setState(() {
+        _playerState = PlayerState.stopped;
+        _position = Duration.zero;
+        playButtonPressed = _isPaused ? true : false;
+        changeIcon(playButtonPressed);
+        playTts.setPlayerState = PlayerState.completed;
+      });
+    });
+
+    _playerStateChangeSubscription =
+        audioPlayer.onPlayerStateChanged.listen((state) {
+      setState(() {
+        _playerState = state;
+        playTts.setPlayerState = state;
+      });
+    });
+  }
+
+  Future<void> _play() async {
+    final position = _position;
+    if (position != null && position.inMilliseconds > 0) {
+      await audioPlayer.seek(position);
+    }
+    await audioPlayer.resume();
+    setState(() => _playerState = PlayerState.playing);
+  }
+
+  Future<void> _pause() async {
+    await audioPlayer.pause();
+    setState(() => _playerState = PlayerState.paused);
+  }
+
+  Future<void> _stop() async {
+    await audioPlayer.stop();
+    setState(() {
+      _playerState = PlayerState.stopped;
+      _position = Duration.zero;
+    });
+  }
+}
